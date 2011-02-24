006be7a2a88de9f576b4e6c2313394bff18bc44d
Production <elianderson03@gmail.com>
2011-02-23 19:40:23 -0800

    clean

--- db/schema.rb
+++ db/schema.rb
@@ -65,10 +65,10 @@ ActiveRecord::Schema.define(:version => 20110213005507) do
   create_table "page_translations", :force => true do |t|
     t.integer  "page_id"
     t.string   "locale"
-    t.string   "browser_title"
-    t.string   "meta_keywords"
     t.text     "meta_description"
+    t.string   "browser_title"
     t.string   "title"
+    t.string   "meta_keywords"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
