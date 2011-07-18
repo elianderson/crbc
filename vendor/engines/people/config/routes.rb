0414270c84f723eab9936216bf963d277c239050
Taylor <taylor@roundhouseagency.com>
2011-07-17 20:12:00 -0700

    added twitter link

--- vendor/engines/people/config/routes.rb
+++ vendor/engines/people/config/routes.rb
@@ -1,6 +1,5 @@
 Refinery::Application.routes.draw do
   resources :people
-
   scope(:path => 'refinery', :as => 'admin', :module => 'admin') do
     resources :people, :except => :show do
       collection do
